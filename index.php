00c51929b30a6611f2cd7899ad45c97276da1cfc
Tech4All <tech4all583@gmail.com>
2021-03-10 11:54:42 +0100

    initial commit

--- index.php
+++ index.php
@@ -5,3 +5,9 @@
  * Date: 3/10/21
  * Time: 10:41 AM
  */
+
+require_once 'config/core.php';
+require_once 'lib/head.php';
+?>
+
+<?php require_once 'lib/foot.php';?>
---
+++ lib/head.php
@@ -0,0 +1,10 @@
+<!Doctype html>
+<html>
+<head>
+    <meta property="og:locale" content="en_US">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <title><?= page_title(@$page_title) ?></title>
+</head>
+<body>
+
